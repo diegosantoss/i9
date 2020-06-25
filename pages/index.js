3ce6cb93571741d74ca68a48160d10f33e5532e6
Diego Santos <dsaantos1995@gmail.com>
2020-06-25 19:37:47 -0300

    updated

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About Us</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/post/[pid]" as="/post/abc">
+            <a>post abc</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/[service]" as="/copeira">
+            <a>copeira</a>
+          </Link>
+        </li>
+      </ul>
+      
+ 
+      <main>
+      </main>
+
+      <footer> 
+      </footer>
+
+    </div>
+  )
+}
