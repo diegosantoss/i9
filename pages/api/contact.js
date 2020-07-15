c4dc57e732e73bf063f7130caab689dc317ede2b
Diego Santos <dsaantos1995@gmail.com>
2020-07-15 09:51:18 -0300

    updated

---
+++ pages/api/contact.js
@@ -0,0 +1,43 @@
+const mailer = require('nodemailer-promise');
+
+const nextSendMail = (req, res) => {
+  const { email, name, phone, subject, message } = req.body;
+    
+  var sendEmail = mailer.config({
+    host: 'smtp.gmail.com',
+    auth: {
+      user: 'i9SiteContact@gmail.com',
+      pass: 'i@Ka13wf9aqg2#123'
+    }
+  });
+
+  const html = `
+    <p>Nome: ${name}</p>
+    <p>Email: ${email}</p>
+    <p>Telefone: ${phone}</p>
+    <p>Assunto: ${subject}</p>
+    <br />
+    <p>Mensagem: ${message}</p>
+  `;
+
+  const mailOptions = {
+    from: name + ' <' + email + '>',
+    to: 'dsaantos1995@gmail.com',
+    replyTo: email,
+    subject,
+    html
+  }
+
+  sendEmail(mailOptions)
+    .then(function(info){
+      // console.log(info);
+      return res.status(200).send({message: 'ok'});
+    })   
+    .catch(function(err){
+      // console.log('got error'); 
+      // console.log(err)
+      return res.status(200).send({message: 'not ok'});
+    })
+} 
+
+module.exports = nextSendMail;
